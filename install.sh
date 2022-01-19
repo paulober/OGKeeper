8882e65f3b45ab93fde9538667d17e3fdd5383d0
pavll <44974737+pavll@users.noreply.github.com>
2022-01-19 20:03:07 +0100

    Version uno

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0]
+    then echo "Please run this installer as root"
+    exit
+fi
+
+cp ogkeeper.py /usr/local/bin/ogkeeper
+cp ogkeeper.service /etc/systemd/system/
+mkdir /etc/ogkeeper
+cp config.json /etc/ogkeeper/
+
+nano /etc/ogkeeper/config.json
---
+++ ogkeeper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import json
+import shutil
+import sys
+import time
+
+def main(config_file="/etc/ogkeeper/config.json"):
+    with open(config_file, 'r') as f:
+        content = ''.join(f.readlines())
+
+    config = json.loads(content)
+
+    time.sleep(int(config["countdownInMinutesNotFloatingpoint"])*60)
+
+    for i in config["keeping"]:
+        with open(i["og"], 'rb') as fsrc, open(i["newfile"], 'wb+') as fdst:
+            fdst.truncate()
+            shutil.copyfileobj(fsrc, fdst)
+
+if __name__ == '__main__':
+    assert len(sys.argv) > 1
+    main(sys.argv[1])
